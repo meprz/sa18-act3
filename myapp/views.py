7ef633c8c5d6cf317dc311b5a2b3c9892bec4aff
Miguel Perez <migeperezjr@gmail.com>
2024-04-23 00:25:18 -0500

    finished act3

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Product
 
 # Create your views here.
+def product_index(request):
+    products = Product.objects.all()
+    return render(request, 'products/index.html', {'products': products})
+
+def product_show(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'products/show.html', {'product': product})
--- storeapp/urls.py
+++ storeapp/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', views.product_index, name='product_index'),
+    path('products/<int:id>/', views.product_show, name='product_show')
 ]
